8aa4b70da8ffa5a7b3f10bf8bafff3ea0b8d6515
mir-toufik <mirtoufikali61@gmail.com>
2024-05-31 17:42:32 +0530

    learn loop in java

---
+++ Loops.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Loops {
+
+    public static void main(String args[]) {
+
+        //for(int i = 10; i>=0;i--) {
+            //System.out.println(i+" ");
+            
+       // }
+
+      // int i =0;
+      // while (i<11) {
+        //System.out.println(i);
+       // i++;
+       //}
+
+          
+       //int i =0;
+      // do{
+       // System.out.println(i);
+       // i++;
+      // } 
+      // while (i<11);
+
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      
+      for(int i=1;i<=10; i++) {
+        System.out.println(i*n);
+
+       }
+    }
+}
