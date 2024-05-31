a456ce5bda12dbaa74384b972380964d1fdfa22b
mir-toufik <mirtoufikali61@gmail.com>
2024-05-31 17:31:37 +0530

    java fun for even or odd

---
+++ fastcode.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class fastcode
+ {
+
+    public static void main(String args[]) {
+        Scanner Sc = new Scanner(System.in);
+        int x = Sc.nextInt();
+
+        if(x % 2== 0){
+            System.out.println("even");
+
+        }else{
+            System.out.println("odd");
+        }
+
+    }
+}
