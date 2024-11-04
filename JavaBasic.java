5ed8af39dbbfc7d7713eb173f6f7028332a0fec5
mir-toufik <mirtoufikali61@gmail.com>
2024-11-05 00:39:53 +0530

    practice

---
+++ JavaBasic.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+ public class JavaBasic{
+    public void main(String args[]){
+
+        Scanner Scanner = new Scanner(System.in);
+        int number = Scanner.nextInt();
+        System.out.println(number+number);
+    }
+ }
--- LargestofThree.java
+++ LargestofThree.java
@@ -1,21 +1,13 @@
 import java.util.Scanner;
 
-public class LargestOfThree {
+public class LargestOfThree{
     public static void main(String[] args) {
-        Scanner Scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
 
-        System.out.print("Enter first number: ");
-        int num1 = Scanner.nextInt();
-        System.out.print("Enter second number: ");
-        int num2 = Scanner.nextInt();
-        System.out.print("Enter third number: ");
-        int num3 = Scanner.nextInt();
+        int number = sc.nextInt();
+         System.out.println(number+number);
 
-        int largest = num1;
-        if (num2 > largest) largest = num2;
-        if (num3 > largest) largest = num3;
-
-        System.out.println("The largest number is: " + largest);
+        
     }
 }
 
